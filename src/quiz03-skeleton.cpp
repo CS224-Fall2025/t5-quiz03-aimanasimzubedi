6b47b4c398ce4cef15d86676f9ed9aebbb6008da
Aiman Asim Zubedi <az10271@st.habib.edu.pk>
2025-11-14 09:23:18 +0500

    changes

--- src/quiz03-skeleton.cpp
+++ src/quiz03-skeleton.cpp
@@ -1,5 +1,68 @@
 #include <iostream>
+using namespace std;
 
+class Employee{
+    protected:
+        string name;
+        double salary;
+    public:
+        static int employeeCount;
+        Employee(string n, double s) {
+        name = n;
+        salary= s;
+        employeeCount++;
+        }
+        virtual void displayInfo(){
+            cout << "Employee Name: " << name << endl;
+            cout << "Employee Salary: " << salary << endl;
+        }
+        static void showTotalEmployees(){
+            return employeeCount;
+        }
+        virtual ~Employee() {
+        // virtual destructor
+        }
+};
+class Staff: public Employee{
+    private:
+        string department;
+        void displayInfo() override {
+            cout<< "Staff:"<<endl;
+            cout << "Employee Name: " << name << endl;
+            cout << "Employee Salary: " << salary << endl;
+        }
+};
+class Faculty: public Employee{
+    private:
+        string department;
+        void displayInfo() override {
+            cout<< "Faculty:"<<endl;
+            cout << "Employee Name: " << name << endl;
+            cout << "Employee Salary: " << salary << endl;
+        }
+};
 int main() {
-
+    int n;
+    cout<<"enter number of employees"<<endl;
+    cin>>n;
+    string t, n;
+    int s;
+    for (i=0; i<n; i++){
+        cout<<"staff or faculty?";
+        cin>>t;
+        cout<<"name:";
+        cin>>n;
+        cout<<"salary:";
+        cin>>s;
+    }
+    cout<<"input end"<<endl;
+    for (i=0; i<n; i++){
+        if (t=="staff"){
+            Staff::displayInfo(n, s){}
+        }elseif (t=="Faculty")[
+            Faculty::displayInfo(n, s){}
+        ]
+    }
+    cout<<"Total Employees Created:"<<Employee::showTotalEmployees()<<endl;
+    return 0;
 }
